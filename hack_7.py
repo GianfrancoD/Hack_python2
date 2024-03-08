40c64b990ccaad578b44a30fa9ced1689d133b00
GianfrancoD <gianfranco.dfiore@gmail.com>
2024-03-08 14:04:49 -0400

    HACK 2 Python

--- hack_7.py
+++ hack_7.py
@@ -8,7 +8,7 @@ text: ["a","b","c","d","e"] output => ["1",2,"3",4,"5"]
 text: [] output => [0] 
 """
 
-
+"""
 def fn_hack_7(s):
     result = s
     for i in range(len(result)):
@@ -22,6 +22,25 @@ def fn_hack_7(s):
             result[i] = 4
         elif result[i] == "e":
             result[i] = "5"
+    return result
+
+fn_hack_7(["a", "b", "c", "d", "e"])
+"""
+def fn_hack_7(s):
+    result = s
+    i = 0
+    while i < len(result):
+        if result[i] == "a":
+            result[i] = "1"
+        elif result[i] == "b":
+            result[i] = 2
+        elif result[i] == "c":
+            result[i] = "3"
+        elif result[i] == "d":
+            result[i] = 4
+        elif result[i] == "e":
+            result[i] = "5"
+        i += 1
     return result
 
 fn_hack_7(["a", "b", "c", "d", "e"])
